639e099544721e65a09d254b3c9d1e38014f6c98
Rafael <rafael.pinedop@gmail.com>
2022-10-31 14:47:20 -0500

    RPP

--- SisGeFin.Reporting.DAT/Codigo.cs
+++ SisGeFin.Reporting.DAT/Codigo.cs
@@ -11,9 +11,7 @@ namespace SisGeFin.Reporting.DAT
         {
             //    <add name="SisGeFinEntities"
             //    connectionString="metadata=res://*/SisGeFinModel.csdl|res://*/SisGeFinModel.ssdl|res://*/SisGeFinModel.msl;provider=System.Data.SqlClient;provider
-            //
             //    connection string=&quot;
-            //
             //    data source=154.53.44.5\SQLEXPRESS;
             //    initial catalog=BDSISGEFIN;
             //    persist security info=True;
@@ -24,10 +22,17 @@ namespace SisGeFin.Reporting.DAT
 
             EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder();
             entityBuilder.Metadata = "res://*/SisGeFinModel.csdl|res://*/SisGeFinModel.ssdl|res://*/SisGeFinModel.msl";
+            
             string _servidor = Environment.GetEnvironmentVariable("SERV_IAFAS", EnvironmentVariableTarget.Machine);
             string _basedato = Environment.GetEnvironmentVariable("DABA_IAFAS", EnvironmentVariableTarget.Machine);
             string _usuariox = Environment.GetEnvironmentVariable("USER_IAFAS", EnvironmentVariableTarget.Machine);
             string _password = Environment.GetEnvironmentVariable("PASS_IAFAS", EnvironmentVariableTarget.Machine);
+
+            //string _servidor = Environment.GetEnvironmentVariable("SERV_IAFAS", EnvironmentVariableTarget.User);
+            //string _basedato = Environment.GetEnvironmentVariable("DABA_IAFAS", EnvironmentVariableTarget.User);
+            //string _usuariox = Environment.GetEnvironmentVariable("USER_IAFAS", EnvironmentVariableTarget.User);
+            //string _password = Environment.GetEnvironmentVariable("PASS_IAFAS", EnvironmentVariableTarget.User);
+
             string _connectn = $"data source={_servidor};initial catalog={_basedato};user id={_usuariox};password={_password};MultipleActiveResultSets=True";
             entityBuilder.ProviderConnectionString = _connectn;
             entityBuilder.Provider = "System.Data.SqlClient";
