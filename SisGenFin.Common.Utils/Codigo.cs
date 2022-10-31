4bf536a6a9ac644d6be4b26f3b71dc904fac0511
Rafael <rafael.pinedop@gmail.com>
2022-10-31 13:38:11 -0500

    RPP
    Reportes

---
+++ SisGenFin.Common.Utils/Codigo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SisGeFin.Common.Utils
+{
+    public static class Utils
+    {
+
+        public static List<T> GetTramaToList<T>(string trama)
+        {
+            var _items = new List<T>();
+            string[] _rows = trama.Split('¬');
+            for (int f = 0; f < _rows.Length; f++)
+            {
+                try
+                {
+                    var _itm = Activator.CreateInstance<T>();
+                    var _row = _rows[f];
+                    string[] _cols = _row.Split('|'); int c = -1;
+                    dynamic _properties = _itm.GetType().GetProperties();
+                    foreach (var _prp in _properties)
+                    {
+                        try
+                        {
+                            c = c + 1;
+                            dynamic _valor = _cols[c];
+                            _prp.SetValue(_itm, Convert.ChangeType(_valor, _prp.PropertyType), null);
+                        }
+                        catch 
+                        {
+                            continue;
+                        }
+                    }
+                    _items.Add(_itm);
+                }
+                catch 
+                {
+                    continue;
+                }
+            }
+            return _items;
+        }
+
+        public static dynamic GetTramaToList(string trama, Type t)
+        {
+            var _listType = typeof(List<>);
+            var _constrc = _listType.MakeGenericType(t);
+            var _items = (System.Collections.IList)Activator.CreateInstance(_constrc);
+            string[] _rows = trama.Split('¬');
+            for (int f = 0; f < _rows.Length; f++)
+            {
+                try
+                {
+                    var _itm = Activator.CreateInstance(t);
+                    var _row = _rows[f];
+                    string[] _cols = _row.Split('|'); int c = -1;
+                    dynamic _properties = _itm.GetType().GetProperties();
+                    foreach (var _prp in _properties)
+                    {
+                        try
+                        {
+                            c = c + 1;
+                            dynamic _valor = _cols[c];
+                            _prp.SetValue(_itm, Convert.ChangeType(_valor, _prp.PropertyType), null);
+                        }
+                        catch 
+                        {
+                            continue;
+                        }
+                    }
+                    _items.Add(_itm);
+                }
+                catch 
+                {
+                    continue;
+                }
+            }
+            return _items;
+        }
+
+        public static byte[] StreamToArray(Stream stream)
+        {
+            var memory = new MemoryStream();
+            memory.CopyTo(stream);
+            return memory.ToArray();
+        }
+
+    }
+
+}
--- WebAppSISGEFIN/rptViewer.aspx
+++ WebAppSISGEFIN/rptViewer.aspx
@@ -3,7 +3,7 @@
 <%@ Register assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
 
 <form id="form1" runat="server">
-    <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" ReuseParameterValuesOnRefresh="True" ToolPanelView="None" DisplayStatusbar="False" EnableDatabaseLogonPrompt="False" EnableTheming="False" HasCrystalLogo="False" HasToggleParameterPanelButton="False" Width="100%" EnableParameterPrompt="False" BestFitPage="False" GroupTreeStyle-BorderStyle="None" GroupTreeStyle-ShowLines="False" HasDrillUpButton="False" HasPageNavigationButtons="False" Height="100%" BorderStyle="None" />
+    <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" ReuseParameterValuesOnRefresh="True" ToolPanelView="None" DisplayStatusbar="False" EnableDatabaseLogonPrompt="False" EnableTheming="False" HasCrystalLogo="False" HasToggleParameterPanelButton="False" Width="100%" EnableParameterPrompt="False" BestFitPage="False" GroupTreeStyle-BorderStyle="None" GroupTreeStyle-ShowLines="False" HasPageNavigationButtons="False" Height="100%" BorderStyle="None" />
 
 </form>
 
